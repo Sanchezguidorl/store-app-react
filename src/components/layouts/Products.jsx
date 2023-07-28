a455db7543135558e9adf87f4297f843ed6a9477
Sanchezguidorl <sanchezguidorl@gmail.com>
2023-07-28 15:53:58 -0300

    Se crea rama para trabajar componente de paginación

--- src/components/layouts/Products.jsx
+++ src/components/layouts/Products.jsx
@@ -8,11 +8,12 @@ function Products() {
   const { productsData, loading, error } =useGetProducts();
   const [currentPage, setCurrentPage] = useState(2);
   const {products, pages}=usePagination(productsData, currentPage);
-
   const pagesDiv= pages.map((page)=> <h1 key={page}> Hola {page}</h1>);
+  
   return (
 <Row className="my-5 g-2 justify-content-center">
-      {!loading ?<>{ products.map((product) =>
+      {!loading ?error? error : <>{ 
+      products.map((product) =>
       <Col key={product.id} xs={12} sm={6} md={4} lg={3} className="p-0  d-flex justify-content-center">
       <CardComponent product={product} />
       </Col>
